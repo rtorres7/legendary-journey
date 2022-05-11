04d4f423c64d73d7cdb6f3ab6e12c5ed990cb223
Julie <julieluckanavage@gmail.com>
2022-05-11 11:49:58 -0400

    minor changes to comments form

--- client/src/components/article/CommentForm.vue
+++ client/src/components/article/CommentForm.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
-        <div v-if="comments.length > 0" class="bg-slate-50 dark:bg-slate-800 energy:bg-gray-600 rounded-lg shadow-md p-4">
+        <div class="bg-slate-50 dark:bg-slate-800 energy:bg-gray-600 rounded-lg shadow-md p-4 mt-8">
             <p class="font-bold mb-4">Discussion</p>
-            <div v-for="comment in comments" :key="comment" class="mb-4">
-                <p class="">Jane Doe</p>
-                <p>(U) {{comment}}</p>
+            <div v-if="comments.length > 0">
+                <div v-for="comment in comments" :key="comment" class=" border-b-2 border-slate-400 mb-4">
+                    <p>Jane Doe</p>
+                    <p>(U) {{comment}}</p>
+                </div>
+            </div>
+            <div v-else>
+                <p class="italic">No comments yet</p>
             </div>
         </div>
-        <form 
+        <form @submit.prevent="onSubmit"
             class="
                 bg-slate-50 dark:bg-slate-800 energy:bg-gray-600 
                 mt-8
@@ -35,11 +40,22 @@
             <input id="acknowledgement" class="cursor-pointer" required type="checkbox" v-model="acknowledgement">
             <label for="acknowledgement" class="p-2 text-sm">I acknowledge that my comment is professional in nature and adheres to the commenting guidelines.</label>
             <select id="classification" class="block bg-transparent border-b-2 border-slate-400 cursor-pointer mt-8 text-sm" required v-model="classification">
-                <option value="" disabled hidden selected>Please Classify...</option>
-                <option value="U">Unclassified</option>
-                <option value="notU">Not Unclassified</option>
+                <option value="" disabled hidden>Please Classify...</option>
+                <option value="U" class="dark:text-black energy:text-black">Unclassified</option>
+                <option value="notU" class="dark:text-black energy:text-black">Not Unclassified</option>
             </select>
-            <input type="submit" @click="onSubmit" class="border-2 border-slate-400 cursor-pointer mt-8 p-2 rounded-md shadow-md text-sm">
+            <input type="submit" 
+                class="
+                    border-2 border-slate-400 
+                    cursor-pointer 
+                    hover:bg-slate-200 dark:hover:bg-slate-700/80 energy:hover:bg-gray-700
+                    mt-8 
+                    p-2 
+                    rounded-md 
+                    shadow-md 
+                    text-sm
+                "
+            >
         </form>
     </div>
 </template>
@@ -56,7 +72,7 @@
         },
         methods: {
             onSubmit() {
-                if (this.comment != '' && this.acknowledgement) {
+                if (this.comment != '' && this.acknowledgement && this.classification != '') {
                     this.comments.push(this.comment)
                     this.comment = ''
                     this.acknowledgement = false
