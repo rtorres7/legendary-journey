486006cebbf5be41d528e7199fe60f1ecc33c971
Julie <julieluckanavage@gmail.com>
2022-06-07 19:24:33 -0400

    console no longer giving popover warning

--- client/src/components/application/AppBanner.vue
+++ client/src/components/application/AppBanner.vue
@@ -162,33 +162,35 @@
                         leave-from-class="transform opacity-100 scale-100"
                         leave-to-class="transform opacity-0 scale-95"
                       >
-                        <PopoverPanel class="absolute -left-96 z-10 mt-3 w-screen -translate-x-1/2 transform px-4 lg:max-w-8xl">
-                          <div class="
-                            lg:w-full
-                            w-40
-                            rounded-md
-                            shadow-2xl
-                            py-2
-                            ring-1 ring-black ring-opacity-5
-                            focus:outline-none
-                            text-sm text-slate-700
-                            font-semibold
-                            bg-mission-gray
-                            dark:bg-slate-700
-                            energy:bg-gray-700
-                            dark:ring-0 dark:highlight-white/5 dark:text-slate-300
-                            energy:text-gray-300
-                            flex
-                          ">
-                            <div class="m-4 w-2/3">
-                              <RegionsListBox class="mb-6"/>
-                              <RegionsMap aria-hidden="true"/>
+                        <div>
+                          <PopoverPanel class="absolute -left-96 z-10 mt-3 w-screen -translate-x-1/2 transform px-4 lg:max-w-8xl">
+                            <div class="
+                              lg:w-full
+                              w-40
+                              rounded-md
+                              shadow-2xl
+                              py-2
+                              ring-1 ring-black ring-opacity-5
+                              focus:outline-none
+                              text-sm text-slate-700
+                              font-semibold
+                              bg-mission-gray
+                              dark:bg-slate-700
+                              energy:bg-gray-700
+                              dark:ring-0 dark:highlight-white/5 dark:text-slate-300
+                              energy:text-gray-300
+                              flex
+                            ">
+                              <div class="m-4 w-2/3">
+                                <RegionsListBox class="mb-6"/>
+                                <RegionsMap aria-hidden="true"/>
+                              </div>
+                              <div class="p-4 w-1/3">
+                                <RegionsList/>
+                              </div>
                             </div>
-                            <div class="p-4 w-1/3">
-                              <RegionsList/>
-                            </div>
-                          </div>
-                        </PopoverPanel>
+                          </PopoverPanel>
+                        </div>
                       </transition>
                   </Popover>
                 </li>
@@ -607,29 +609,31 @@
                         leave-from-class="transform opacity-100 scale-100"
                         leave-to-class="transform opacity-0 scale-95"
                       >
-                        <PopoverPanel class="absolute left-2/3 md:left-3/4 lg:hidden z-10 mt-3 w-96 md:w-max -translate-x-1/2 transform px-4">
-                          <div class="
-                            rounded-md
-                            shadow-2xl
-                            py-2
-                            p-2
-                            ring-1 ring-black ring-opacity-5
-                            focus:outline-none
-                            text-sm text-slate-700
-                            font-semibold
-                            bg-mission-gray
-                            dark:bg-slate-700
-                            energy:bg-gray-700
-                            dark:ring-0 dark:highlight-white/5 dark:text-slate-300
-                            energy:text-gray-300
-                            flex flex-col
-                            space-y-6
-                          ">
-                              <RegionsListBox class="mt-2"/>
-                              <RegionsList class="pt-4 border-t border-slate-900/10 dark:border-slate-50/[0.06] energy:border-gray-700/25"/>
-                              <RegionsMap aria-hidden="true" class="pt-4 border-t border-slate-900/10 dark:border-slate-50/[0.06] energy:border-gray-700/25"/>
-                          </div>
-                        </PopoverPanel>
+                        <div>
+                          <PopoverPanel class="absolute left-2/3 md:left-3/4 lg:hidden z-10 mt-3 w-96 md:w-max -translate-x-1/2 transform px-4">
+                            <div class="
+                              rounded-md
+                              shadow-2xl
+                              py-2
+                              p-2
+                              ring-1 ring-black ring-opacity-5
+                              focus:outline-none
+                              text-sm text-slate-700
+                              font-semibold
+                              bg-mission-gray
+                              dark:bg-slate-700
+                              energy:bg-gray-700
+                              dark:ring-0 dark:highlight-white/5 dark:text-slate-300
+                              energy:text-gray-300
+                              flex flex-col
+                              space-y-6
+                            ">
+                                <RegionsListBox class="mt-2"/>
+                                <RegionsList class="pt-4 border-t border-slate-900/10 dark:border-slate-50/[0.06] energy:border-gray-700/25"/>
+                                <RegionsMap aria-hidden="true" class="pt-4 border-t border-slate-900/10 dark:border-slate-50/[0.06] energy:border-gray-700/25"/>
+                            </div>
+                          </PopoverPanel>
+                        </div>
                       </transition>
                     </Popover>
                     <a v-else
