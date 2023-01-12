a2660e239153afe999ad1d4d500625bcecde775c
Alvaro Ramirez <aramirezswe@gmail.com>
2023-01-11 21:03:26 -0500

    stuff

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,12 +1,19 @@
 export const isEmpty = (value) => {
-  return value === undefined ||
+  return (
+    value === undefined ||
     value === null ||
     (typeof value === "object" && Object.keys(value).length === 0) ||
     (typeof value === "string" && value.trim().length === 0)
-}
+  );
+};
 export const getValueForCode = (list, code) => {
   return list.find((item) => item.code === code);
 };
 export const getValueForName = (list, name) => {
   return list.find((item) => item.name === name);
+};
+
+export const transformWireUrl = (wireDoc) => {
+  //regex to block WIRe prefix should go here
+  return wireDoc;
 };
