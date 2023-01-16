8b324bbaebc0968b3ec95bb555f58f444c445c9e
Alvaro Ramirez <aramirezswe@gmail.com>
2023-01-16 15:22:26 -0500

    vue config had a typo

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,48 +1,45 @@
-const { defineConfig } = require('@vue/cli-service')
-const path = require( 'path' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const CKEditorWebpackPlugin = require("@ckeditor/ckeditor5-dev-webpack-plugin");
+const { styles } = require("@ckeditor/ckeditor5-dev-utils");
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  transpileDependencies: [
-    /ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/,
-  ],
+  transpileDependencies: [/ckeditor5-[^/\\]+[/\\]src[/\\].+\.js$/],
   configureWebpack: {
     plugins: [
-      new CKEditorWebpackPlugin( {
-          language: 'en',
+      new CKEditorWebpackPlugin({
+        language: "en",
 
-          translationsOutputFile: /app/
-      } )
-    ]
+        translationsOutputFile: /app/,
+      }),
+    ],
   },
 
-    chainWebpack: config => {
-      const svgRule = config.module.rule( 'svg' );
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
 
-      svgRule.exclude.add( path.join( __dirname, 'node_modules', '@ckeditor' ) );
+    svgRule.exclude.add(path.join(__dirname, "node_modules", "@ckeditor"));
 
-      config.module
-        .rule( 'cke-svg' )
-        .test( /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/ )
-        .use( 'raw-loader' )
-        .loader( 'raw-loader' );
-      
-      config.module
-        .rule( 'cke-css' )
-        .test( /ckeditor5-[^/\\]+[/\\].+\.css$/ )
-        .use( 'postcss-loader' )
-        .loader( 'postcss-loader' )
-        .tap( () => {
-            return {
-                postcssOptions: styles.getPostCssConfig( {
-                    themeImporter: {
-                        themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' ),
-                    },
-                    minify: true
-                } )
-            };
-        } );
-    }
-})
+    config.module
+      .rule("cke-svg")
+      .test(/ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/)
+      .use("raw-loader")
+      .loader("raw-loader");
+
+    config.module
+      .rule("cke-css")
+      .test(/ckeditor5-[^/\\]+[/\\].+\.css$/)
+      .use("postcss-loader")
+      .loader("postcss-loader")
+      .tap(() => {
+        return {
+          postcssOptions: styles.getPostCssConfig({
+            themeImporter: {
+              themePath: require.resolve("@ckeditor/ckeditor5-theme-lark"),
+            },
+            minify: true,
+          }),
+        };
+      });
+  },
+});
