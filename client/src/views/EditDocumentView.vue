5c789a3c8f837296575c59bb41d28263a4faa9f6
Julie Luckanavage <jluckanavage@avantusfederal.com>
2023-01-17 17:37:44 -0500

    these few things were changed on the high side today

--- client/src/views/EditDocumentView.vue
+++ client/src/views/EditDocumentView.vue
@@ -578,14 +578,11 @@ export default {
       },
       simpleUpload: {
         //the URL that images are uploaded to.
-        uploadUrl: "/documents/" + documentNumber + "/attachments/",
+        uploadUrl: "/documents/" + documentNumber + "/attachments?is_visible=false",
         //Enable the XMLHttpRequest.withCredentials property.
         withCredentials: true,
         //Headers sent along with the XMLHttpRequest to the upload server.
-        headers: {
-          "X-CSRF-TOKEN": "CSRF-Token",
-          Authorization: "Bearer <JSON Web Token",
-        },
+        headers: {},
       },
     });
     const { files, addFiles, removeFile } = useFileList();
