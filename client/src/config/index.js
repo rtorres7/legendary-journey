9a1e81771545ccead0c847554adb6efa6021df8d
Alvaro Ramirez <aramirezswe@gmail.com>
2023-03-12 21:15:24 -0400

    forgot to tweak the config accordingly

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,9 @@
-//export const metadata = `./${import.meta.env.MODE}.metadata.json`;
-import test from "./development.metadata.json";
-export const metadata = test;
+import lowData from "./low.metadata.json";
+import devData from "./development.metadata.json";
+let data;
+if (import.meta.env.MODE === "low") {
+  data = lowData;
+} else {
+  data = devData;
+}
+export const metadata = data;
