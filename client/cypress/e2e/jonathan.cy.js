c814601e185edaad3d062e7a9dd0d46ed1a1420c
Alvaro Ramirez <aramirezswe@gmail.com>
2023-04-17 13:57:11 -0400

    reviewed/feedback applied

--- client/cypress/e2e/jonathan.cy.js
+++ client/cypress/e2e/jonathan.cy.js
@@ -1,28 +1,29 @@
-describe("Search", () => {
+describe("Low Side Search", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:8080/");
+    cy.visit("");
+    cy.get("#typeahead_id").type("Zelensky{enter}");
   });
 
   it("navigate to the right page", () => {
-    cy.get("#typeahead_id").type("Zelensky");
-    cy.get('#search-container').find('button').first().click();
     cy.url().should("include", "/search?text=");
   });
 
   it("displays search results", () => {
-    cy.get("#typeahead_id").type("Zelensky");
-    cy.get('#search-container').find('button').first().click();
     cy.contains("Inside Zelensky's World");
   });
 
   it("displays search count", () => {
-    cy.get("#typeahead_id").type("Zelensky");
-    cy.get('#search-container').find('button').first().click();
     cy.contains("Showing 1");
+  });
+});
+
+describe("High Side Search", () => {
+  beforeEach(() => {
+    cy.visit("");
   });
 
   it("verify open ended search brings back result", () => {
-    cy.get('#search-container').find('button').first().click();
+    cy.get("#search-container").find("button").first().click();
     cy.contains("Result 1 of");
   });
 });
