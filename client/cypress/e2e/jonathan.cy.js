005770bbc9f42637a42bc9a825901fa7b1a553a2
Jonathan <jonathanjeep@yahoo.com>
2023-04-17 12:52:04 -0400

    first attempt at basic search

--- client/cypress/e2e/jonathan.cy.js
+++ client/cypress/e2e/jonathan.cy.js
@@ -1,14 +1,28 @@
 describe("Search", () => {
   beforeEach(() => {
     cy.visit("http://localhost:8080/");
-    cy.get("#typeahead_id").type("Zelensky{enter}");
   });
 
   it("navigate to the right page", () => {
+    cy.get("#typeahead_id").type("Zelensky");
+    cy.get('#search-container').find('button').first().click();
     cy.url().should("include", "/search?text=");
   });
 
   it("displays search results", () => {
+    cy.get("#typeahead_id").type("Zelensky");
+    cy.get('#search-container').find('button').first().click();
     cy.contains("Inside Zelensky's World");
+  });
+
+  it("displays search count", () => {
+    cy.get("#typeahead_id").type("Zelensky");
+    cy.get('#search-container').find('button').first().click();
+    cy.contains("Showing 1");
+  });
+
+  it("verify open ended search brings back result", () => {
+    cy.get('#search-container').find('button').first().click();
+    cy.contains("Result 1 of");
   });
 });
