9975985605e0ab20e920d48739000824e46efa36
Alvaro Ramirez <aramirezswe@gmail.com>
2023-05-10 00:05:09 -0400

    need moar icons

--- client/src/views/MockWorkspaceView.vue
+++ client/src/views/MockWorkspaceView.vue
@@ -330,6 +330,7 @@
           class="flex flex-col p-8 text-gray-900 bg-slate-50 w-full min-h-[calc(100vh-4rem)] h-full items-center"
         >
           <div class="max-w-[475px] sm:max-w-[1600px] w-full">
+            <!-- Dashboard View -->
             <div :class="isActivePage('#dashboard') ? 'block' : 'hidden'">
               <div class="text-2xl text-gray-700">Alvaro's Workspace</div>
               <div class="text-gray-400 text-sm">
@@ -345,15 +346,81 @@
                 <div class="text-lg font-bold">Continue where you left off</div>
               </div>
               <div
-                class="flex flex-col bg-white shadow-md rounded max-w-[450px] p-8"
+                class="relative flex flex-col bg-white shadow-md hover:shadow-lg rounded max-w-[450px] p-8 cursor-pointer"
               >
+                <div
+                  class="text-gray-500 hover:text-gray-900 absolute top-3 right-3"
+                >
+                  <Menu as="div" class="relative">
+                    <div>
+                      <tippy content="More" placement="bottom">
+                        <MenuButton
+                          class="max-w-xs rounded-full flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-600"
+                        >
+                          <span class="sr-only">More Options</span>
+                          <EllipsisVerticalIcon
+                            class="h-6 w-6"
+                            aria-hidden="true"
+                          />
+                        </MenuButton>
+                      </tippy>
+                    </div>
+                    <transition
+                      enterActiveClass="transition ease-out duration-100"
+                      enterFromClass="transform opacity-0 scale-95"
+                      enterToClass="transform opacity-100 scale-100"
+                      leaveActiveClass="transition ease-in duration-75"
+                      leaveFromClass="transform opacity-100 scale-100"
+                      leaveToClass="transform opacity-0 scale-95"
+                    >
+                      <MenuItems
+                        class="origin-top-right absolute right-0 mt-2 w-36 rounded-md shadow-lg py-2 text-gray-900 ring-1 bg-white ring-gray-900 ring-opacity-5 focus:outline-none text-sm"
+                      >
+                        <MenuItem>
+                          <div
+                            class="py-2 px-3 hover:bg-gray-100 flex items-center space-x-4 cursor-pointer"
+                          >
+                            <ShareIcon
+                              class="h-5 w-5"
+                              aria-hidden="true"
+                            /><span class="capitalize">Share</span>
+                          </div>
+                        </MenuItem>
+                        <MenuItem>
+                          <div
+                            class="py-2 px-3 hover:bg-gray-100 flex items-center space-x-4 cursor-pointer"
+                          >
+                            <PencilSquareIcon
+                              class="h-5 w-5"
+                              aria-hidden="true"
+                            /><span class="capitalize">Edit</span>
+                          </div>
+                        </MenuItem>
+                        <MenuItem>
+                          <div
+                            class="py-2 px-3 hover:bg-gray-100 flex items-center space-x-4 cursor-pointer"
+                          >
+                            <TrashIcon
+                              class="h-5 w-5"
+                              aria-hidden="true"
+                            /><span class="capitalize">Delete</span>
+                          </div>
+                        </MenuItem>
+                      </MenuItems>
+                    </transition>
+                  </Menu>
+                </div>
                 <div class="flex items-center space-x-4">
                   <MaxProductIcon class="w-8 h-8 text-green-500" icon="globe" />
                   <div class="text-xs text-gray-500">Community</div>
                 </div>
-                <p class="font-semibold line-clamp-2 mt-4">
-                  (U) Canada 'will not be intimidated' after China expels
-                  diplomat: Trudeau
+                <p
+                  class="font-semibold text-gray-700 line-clamp-2 mt-4"
+                  title="(U) Canada 'will not be intimidated' after China expels
+                  diplomat: Trudeau"
+                >
+                  <span class="font-medium text-gray-500">(U)</span> Canada
+                  'will not be intimidated' after China expels diplomat: Trudeau
                 </p>
                 <div class="text-sm text-gray-500 mt-4">15 hours ago</div>
               </div>
@@ -371,15 +438,18 @@
               >
                 <template v-for="n in 4" :key="n">
                   <div
-                    class="bg-white rounded-md max-w-[464px] shadow-md hover:shadow-lg cursor-pointer mb-2"
+                    class="flex flex-col bg-white rounded-md max-w-[464px] shadow-md hover:shadow-lg cursor-pointer mb-2"
                   >
                     <div class="max-h-[261px]">
                       <img src="@/assets/mocks/16x9_001_astronaut.jpg" alt="" />
                     </div>
-                    <div class="flex flex-col py-4">
-                      <div class="relative pb-6 pl-4 pr-6">
+                    <div class="h-full flex flex-col py-4 justify-between">
+                      <div class="relative pb-6 px-4">
+                        <div class="text-xs text-blue-700 font-medium pb-2">
+                          Space Travel
+                        </div>
                         <p
-                          class="font-semibold text-gray-700 line-clamp-2"
+                          class="font-semibold text-gray-700 line-clamp-3"
                           title="(U) Astronaut Buzz Aldrin named honorary brigadier general,
                         member of Space Force"
                         >
@@ -470,21 +540,28 @@
                           </Menu>
                         </div>
                       </div>
-                      <div
-                        class="flex justify-between px-4 text-sm text-gray-500"
-                      >
-                        <div class="flex space-x-4">
+                      <div class="px-4 text-sm text-gray-500">
+                        <div class="flex space-x-2">
                           <div>4 days ago</div>
-                          <div class="text-blue-700 font-medium">
-                            Space Travel
-                          </div>
+                          <div>•</div>
+                          <div>101k views</div>
                         </div>
-                        <div class="text-xs md:text-sm">101k views</div>
                       </div>
                     </div>
                   </div>
                 </template>
               </div>
+              <div class="py-6 flex items-center">
+                <div class="text-lg font-bold">Your Stats</div>
+              </div>
+              <div
+                class="border border-slate-300/70 shadow-sm rounded px-6 py-4 max-w-[400px]"
+              >
+                <span class="text-sm font-normal text-slate-600">
+                  Total Created</span
+                >
+              </div>
+              <!-- <div>Total Views</div> -->
             </div>
           </div>
         </div>
