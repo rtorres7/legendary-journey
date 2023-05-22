71a8eb532c0f010334f4b9dc31d7bd7936523fbe
Julie Luckanavage <jluckanavage@avantusfederal.com>
2023-05-22 14:23:37 -0400

    pega-1061 mock cmt

--- client/src/components/max-ui/core/LowClassifier.vue
+++ client/src/components/max-ui/core/LowClassifier.vue
@@ -11,14 +11,55 @@
     v-bind="$attrs"
     :id="uuid"
     autocomplete="off"
-    :value="modelValue"
+    readonly
+    :value="getClassificationName(modelValue)"
     class="min-h-[2rem] flex w-full rounded-lg cursor-default py-1 px-2 mt-1 bg-transparent disabled:bg-slate-200/50 disabled:dark:bg-slate-700 disabled:energy:bg-zinc-700 border border-gray-300 dark:border-slate-600 energy:border-zinc-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-offset-2 placeholder:italic"
-    @input="updateModels($event.target.value)"
+    @click="openClassifyDialog"
   />
+  <MaxDialog
+    class="max-w-[600px]"
+    :isOpen="isClassifyDialogOpen"
+    @close="closeClassifyDialog"
+  >
+    <div class="flex flex-col gap-y-4">
+      <template v-for="classif in classifs" :key="classif">
+        <div>
+          <input
+            :id="classif.name"
+            :v-model="selectedClassif"
+            type="checkbox"
+            :value="classif.name"
+            class="classifCheckbox"
+            :checked="isChecked(classif)"
+            @change="updateChecked(classif)"
+          />
+          <label :for="classif.name" class="ml-2 text-sm">{{
+            classif.name
+          }}</label>
+        </div>
+      </template>
+    </div>
+    <template #actions>
+      <MaxButton
+        color="secondary"
+        @click="
+          closeClassifyDialog();
+          updateModels(checkedVal);
+        "
+      >
+        Classify
+      </MaxButton>
+      <MaxButton color="secondary" @click="closeClassifyDialog"
+        >Cancel</MaxButton
+      >
+    </template>
+  </MaxDialog>
 </template>
 <script>
 /* eslint-disable no-undef */
 import uniqueID from "@/composables/uniqueID";
+import { computed, ref } from "vue";
+import { useStore } from "vuex";
 export default {
   components: {},
   props: {
@@ -38,20 +79,79 @@ export default {
   emits: ["update:classObj", "update:modelValue"],
   setup(props, { emit }) {
     const uuid = uniqueID().getID();
+    const store = useStore();
+    const classifs = computed(
+      () => store.state.metadata.criteria.classification
+    );
+    const selectedClassif = ref();
+    const checkedVal = ref();
+
+    const updateChecked = (val) => {
+      checkedVal.value = val;
+      //only select one checkbox for now. maybe we'll make it more complex/multiselect in the future to be more like cmt?
+      Array.from(document.getElementsByClassName("classifCheckbox")).forEach(
+        (checkbox) => {
+          if (checkbox.value == val.name || checkbox.value == val.marking) {
+            checkbox.checked = true;
+          } else {
+            checkbox.checked = false;
+          }
+        }
+      );
+    };
+
+    const isChecked = (val) => {
+      const inputValue = document.getElementById(uuid).value;
+      if (inputValue == val.name || inputValue == val.marking) {
+        return true;
+      }
+      return false;
+    };
 
     const updateModels = (val) => {
-      emit("update:modelValue", val);
+      emit("update:modelValue", val.name);
       emit("update:classObj", {
-        xml: val,
-        name: val,
-        marking: val,
-        block: '',
+        xml: val.xml,
+        name: val.name,
+        marking: val.marking,
+        block: "",
       });
+    };
+
+    const isClassifyDialogOpen = ref(false);
+    const openClassifyDialog = () => {
+      isClassifyDialogOpen.value = true;
+    };
+    const closeClassifyDialog = () => {
+      isClassifyDialogOpen.value = false;
+    };
+
+    const getClassificationName = (val) => {
+      if (val) {
+        const classif = classifs.value.find(
+          (c) => c.xml == val || c.name == val
+        );
+        if (props.label == "Document Classification") {
+          return classif?.name;
+        } else {
+          return classif?.marking;
+        }
+      }
+      return "";
     };
 
     return {
       uuid,
+      checkedVal,
+      isChecked,
+      updateChecked,
       updateModels,
+      classifs,
+      selectedClassif,
+      isClassifyDialogOpen,
+      openClassifyDialog,
+      closeClassifyDialog,
+      getClassificationName,
     };
   },
 };
