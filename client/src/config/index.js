7eb1b75d0b65240dd26a8ed2613fd9572add12c1
Alvaro Ramirez <aramirezswe@gmail.com>
2023-06-13 22:13:00 -0400

    importing/fetching metadata tweaks

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,11 +1,15 @@
 let file;
+let data;
 if (
   import.meta.env.MODE === "production" ||
   import.meta.env.MODE === "development"
 ) {
   file = "/production.metadata.json";
+  const fetchedData = await fetch(file);
+  data = fetchedData.json();
 } else {
   file = "./low.metadata.json";
+  const importedData = await import(file);
+  data = importedData;
 }
-const data = await fetch(file);
-export const metadata = await data.json();
+export const metadata = data;
