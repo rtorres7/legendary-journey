5ac85fec31a58d8dc1b823c2e553ef370b9d0c47
Alvaro Ramirez <aramirezswe@gmail.com>
2023-06-13 21:55:45 -0400

    synching changes found in high to avoid breaking

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -3,9 +3,9 @@ if (
   import.meta.env.MODE === "production" ||
   import.meta.env.MODE === "development"
 ) {
-  file = "./production.metadata.json";
+  file = "/production.metadata.json";
 } else {
   file = "./low.metadata.json";
 }
-const data = await import(file);
-export const metadata = data;
+const data = await fetch(file);
+export const metadata = await data.json();
