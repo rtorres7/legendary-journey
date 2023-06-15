180c22fd8e70daace094b49c35edaa976f796a8b
Alvaro Ramirez <aramirezswe@gmail.com>
2023-06-15 11:45:18 -0400

    platform cannot run with my recent change, only works in offline mode so had to undo. Will investigate

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -4,7 +4,7 @@ if (
   import.meta.env.MODE === "development"
 ) {
   //TODO: We have to create a production.metadata.json in the root of the project for this not to fail. We need to figure out a better way in order to avoid this.
-  data = await import("../../../production.metadata.json");
+  //data = await import("../../../production.metadata.json");
 } else {
   data = await import("./low.metadata.json");
 }
